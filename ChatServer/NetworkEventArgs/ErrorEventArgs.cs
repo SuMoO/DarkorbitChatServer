1f1b961589c709ec8bff3248539cdb42129c72b3
C0untLizzi <spammmail55@web.de>
2014-06-15 14:15:19 +0200

    More implementation + events

---
+++ ChatServer/NetworkEventArgs/ErrorEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChatServer.NetworkEventArgs
+{
+    public class ErrorEventArgs : EventArgs
+    {
+        public Client User { get; set; }
+
+        public string Message { get; set; }
+
+        public Exception Error { get; set; }
+    }
+}
---
+++ ChatServer/NetworkEventArgs/ServerErrorEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ChatServer.NetworkEventArgs
+{
+    public class ServerErrorEventArgs : EventArgs
+    {
+        public Exception Error { get; set; }
+
+        public string Message { get; set; }
+    }
+}
